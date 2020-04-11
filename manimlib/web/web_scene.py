70f1df2632acacaa8e24b7a6f8da7c798dc60212
Devin Neal <devin@eulertour.com>
2020-04-10 18:56:41 -0700

    Don't render scenes twice

--- manimlib/web/web_scene.py
+++ manimlib/web/web_scene.py
@@ -2,6 +2,9 @@ from manimlib.scene.scene import PyScene
 
 
 class Scene(PyScene):
+    def __init__(self, **kwargs):
+        self.init_kwargs = kwargs
+
     def render(self):
-        # Regular Scenes render upon instantiation.
-        return super(Scene, self).__init__()
+        # PyScene renders upon instantiation.
+        return super(Scene, self).__init__(**self.init_kwargs)
