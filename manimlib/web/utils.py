092de57ff2444b650d46d7c3dfa0e8845fdcb734
Devin Neal <devin@eulertour.com>
2020-04-13 20:33:53 -0700

    Always send colors as hex strings

--- manimlib/web/utils.py
+++ manimlib/web/utils.py
@@ -1,12 +1,21 @@
 import js
 import pyodide
+from colour import Color
 
 def get_mobject_style(mob):
+    stroke_rgbas = mob.get_stroke_rgbas()
+    fill_rgbas = mob.get_fill_rgbas()
+
+    if len(stroke_rgbas) != 1:
+        print("Multiple stroke rgbas are not supported, using the first one.")
+    if len(fill_rgbas) != 1:
+        print("Multiple fill rgbas are not supported, using the first one.")
+
     return {
-        "strokeColor": str(mob.get_stroke_color()),
-        "strokeOpacity": mob.get_stroke_opacity(),
-        "fillColor": str(mob.get_fill_color()),
-        "fillOpacity": mob.get_fill_opacity(),
+        "strokeColor": Color(rgb=stroke_rgbas[0][:3]).hex_l,
+        "strokeOpacity": stroke_rgbas[0][3],
+        "fillColor": Color(rgb=fill_rgbas[0][:3]).hex_l,
+        "fillOpacity": fill_rgbas[0][3],
         "strokeWidth": mob.get_stroke_width(),
     }
 
